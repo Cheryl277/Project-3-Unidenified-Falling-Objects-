d135c1fb9cb1ca7043b2fbaf12b9c9d7bbf8537b
Jonathan Lilley <jonathan.lilley@bostonsp.com>
2023-04-24 00:02:46 -0400

    heat map

--- Website/html/heat_js/ufo_heat.js
+++ Website/html/heat_js/ufo_heat.js
@@ -1,6 +1,7 @@
 var myMap = L.map("heat_map", {
     center: [39.0997, -94.5786],
-    zoom: 13
+    //layers: [street, ufo],
+    zoom: 5
 });
 
 // Adding the tile layer
@@ -8,20 +9,43 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-var UFOData = "../Resources/ufo_data_clean.json";
+// var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// })
 
-Promise.all([d3.json(UFOData)]).then(function (data) {
+// var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+//     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+// });
+
+// var baseMaps = {
+//     Street: street,
+//     Topography: topo
+// };
+
+// var overlayMaps = {
+//     ufo: heat,
+//     pop_heat: population
+// };
+
+// L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+var UFOData = "../../Resources/ufo_data_clean.json";
+var PopulationData = "../../Resources/population_density.json";
+
+Promise.all([d3.json(UFOData), d3.json(PopulationData)]).then(function (data) {
     //assign variables to each file
     var UFOJSON = data[0];
+    var PopulationJSON = data[1];
+
+    console.log(Object.values(PopulationJSON).map(o => { return o.lat }));
+    console.log(Object.values(PopulationJSON).map(o => { return o.lng }));
 
     var UFOLat = [];
     var UFOLon = [];
-    var UFOCity = [];
 
     for (let j = 0; j < 67052; j++) {
         UFOLat.push(UFOJSON[j].ufo_latitude);
         UFOLon.push(UFOJSON[j].ufo_longitude);
-        UFOCity.push(UFOJSON[j].city);
     };
 
     var c = UFOLat.map(function (e, i) {
@@ -40,5 +64,32 @@ Promise.all([d3.json(UFOData)]).then(function (data) {
         radius: 20,
         blur: 35
     }).addTo(myMap);
+
+    var PopLat = [];
+    var PopLon = [];
+
+    PopLat = Object.values(PopulationJSON).map(o => { return o.lat });
+    PopLon = Object.values(PopulationJSON).map(o => { return o.lng });
+
+    var pop_heatArray = [];
+
+    for (var x = 0; x < PopLat.length; x++) {
+
+        pop_heatArray.push([PopLat[x], PopLon[x]]);
+    }
+
+    console.log(pop_heatArray)
+
+    // var pop_heat = L.heatLayer(pop_heatArray, {
+    //     radius: 20,
+    //     blur: 35,
+    //     gradient: {
+    //         0.2: "#ffffd4",
+    //         0.4: "#fed98e",
+    //         0.6: "#fe9929",
+    //         0.8: "#d95f0e",
+    //         1: "#993404"
+    //     }
+    // }).addTo(myMap);
 
 });
